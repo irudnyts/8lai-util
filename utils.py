de31561020dfd80cce5c34ee1589a2bbdb46c274
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-02-02 15:27:06 +0100

    Add higher-level function to work with models

--- utils.py
+++ utils.py
@@ -9,3 +9,16 @@ def describe_numeric_variable(data, variable):
 
 def describe_categorical_variable(data, variable):
   return data.loc[:, variable].value_counts()
+
+def fit_simple_linear_regression(data, y, x):
+  x = data[x].to_numpy().reshape(-1, 1)
+  y = data[y].to_numpy()
+  model = LinearRegression()
+  model.fit(x, y)
+  return model
+
+def get_coefficients(model):
+  return [model.intercept_, model.coef_[0]]
+
+def predict(model, x):
+  return model.predict(np.array([[x]]))
