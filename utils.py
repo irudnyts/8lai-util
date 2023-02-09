0755bef03e1f703bba1e81dc453715de24b54c12
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-02-09 12:13:32 +0100

    Update functions

--- utils.py
+++ utils.py
@@ -50,8 +50,6 @@ def get_rmse(model, data, y, x):
   y_true = data[y]
   return math.sqrt(statistics.mean((y_pred - y_true) ** 2))
 
-#---------------------------------
-
 def draw_raw(x, i):
   size = int(math.sqrt(x.shape[1]))
   image = tf.transpose(tf.reshape(tf.convert_to_tensor(x.iloc[i, :]), [size, size]))
@@ -60,28 +58,21 @@ def draw_raw(x, i):
   plt.show()
 
 def fit_dense_neural_network(x, y, layers = []):
-
   n_features = int(x.shape[1])
   n_categories = y.value.unique().shape[0]
-
   y = tf.convert_to_tensor(y)
   x = tf.convert_to_tensor(x)
-
   x = tf.divide(x, 255)
-
   model = keras.models.Sequential()
   model.add(keras.layers.Dense(300, activation="relu", input_shape = [n_features]))
   model.add(keras.layers.Dense(100, activation="relu"))
   model.add(keras.layers.Dense(n_categories, activation = "softmax"))
-
   model.compile(
       loss = keras.losses.sparse_categorical_crossentropy,
       metrics = keras.metrics.sparse_categorical_accuracy,
       optimizer = keras.optimizers.RMSprop()
   )
-
   model.fit(x, y, epochs=10, batch_size = 32)
-
   return model
 
 def predict_class(model, x, i):
