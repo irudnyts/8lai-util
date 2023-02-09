babd74691c051ff16ab77cb3a30417784e3eb7f0
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-02-09 12:10:25 +0100

    Add new functions related to neural networks

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import statistics
 import math
+from tensorflow import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def count_number_of_rows(data):
   return len(data.index)
@@ -46,3 +49,41 @@ def get_rmse(model, data, y, x):
   y_pred = model.predict(data[x].to_numpy().reshape(-1, x_length))
   y_true = data[y]
   return math.sqrt(statistics.mean((y_pred - y_true) ** 2))
+
+#---------------------------------
+
+def draw_raw(x, i):
+  size = int(math.sqrt(x.shape[1]))
+  image = tf.transpose(tf.reshape(tf.convert_to_tensor(x.iloc[i, :]), [size, size]))
+  fig = plt.figure
+  plt.imshow(image, cmap='gray')
+  plt.show()
+
+def fit_dense_neural_network(x, y, layers = []):
+
+  n_features = int(x.shape[1])
+  n_categories = y.value.unique().shape[0]
+
+  y = tf.convert_to_tensor(y)
+  x = tf.convert_to_tensor(x)
+
+  x = tf.divide(x, 255)
+
+  model = keras.models.Sequential()
+  model.add(keras.layers.Dense(300, activation="relu", input_shape = [n_features]))
+  model.add(keras.layers.Dense(100, activation="relu"))
+  model.add(keras.layers.Dense(n_categories, activation = "softmax"))
+
+  model.compile(
+      loss = keras.losses.sparse_categorical_crossentropy,
+      metrics = keras.metrics.sparse_categorical_accuracy,
+      optimizer = keras.optimizers.RMSprop()
+  )
+
+  model.fit(x, y, epochs=10, batch_size = 32)
+
+  return model
+
+def predict_class(model, x, i):
+  tensor = tf.reshape(tf.convert_to_tensor(x.iloc[i, :]), [1, int(x.shape[1])])
+  return np.argmax(model.predict(tensor, verbose = 0))
